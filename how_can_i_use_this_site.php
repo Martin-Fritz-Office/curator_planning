32de00b3cf313f86ad90e1a6181fb7fce836d9c2
mfritz0907 <mf@martinfritz.net>
2026-02-18 20:20:13 +0100

    Fix title spacing in HTML document

--- how_can_i_use_this_site.php
+++ how_can_i_use_this_site.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1" />
-  <title>How canI use this site</title>
+  <title>How can I use this site</title>
   <link rel="icon" type="image/svg+xml" href="favicon.svg" />
   <link rel="stylesheet" href="style.css" />
 </head>
